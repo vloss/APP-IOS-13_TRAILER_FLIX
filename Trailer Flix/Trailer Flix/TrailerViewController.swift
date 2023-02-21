7f84d5d23da7c6ea98994ff40012c33d667b80c9
vloss <viniciusvianaloss@gmail.com>
2023-02-21 16:57:59 -0300

    Finalizando App, com execução do video.

--- Trailer Flix/Trailer Flix/TrailerViewController.swift
+++ Trailer Flix/Trailer Flix/TrailerViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVKit
 
 class TrailerViewController: UIViewController {
 
@@ -16,11 +17,49 @@ class TrailerViewController: UIViewController {
     @IBOutlet weak var viTrailer: UIView!
     
     var trailer: Trailer!
+    var player: AVPlayer!
+    var playerController: AVPlayerViewController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        prepareView()
+        preparePlayer()
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: [])
+        }
+        catch {
+            print("Setting category to AVAudioSessionCategoryPlayback failed.")
+        }
+    }
+    
+    func prepareView(){
         lbTitle.text = trailer.title
+        lbYear.text = "\(trailer.year)"
+        var rating = "Ainda não avaliado"
+        
+        if(trailer.rating > 0){
+            rating = ""
+            for _ in 1...trailer.rating {
+                rating += "⭐️"
+            }
+        }
+
+        lbRating.text = rating
+        ivTrailer.image = UIImage(named: trailer.poster + "-large")
+    }
+    
+    func preparePlayer(){
+        let url = URL(fileURLWithPath: trailer.url)
+        player = AVPlayer(url: url)
+        player.volume = 1
+        playerController = AVPlayerViewController()
+        playerController.player = player
+        playerController.showsPlaybackControls = true
+        playerController.player?.play()
+        playerController.view.frame = viTrailer.bounds // view interna
+        viTrailer.addSubview(playerController.view)
     }
     
     @IBAction func close(_ sender: UIButton) {
